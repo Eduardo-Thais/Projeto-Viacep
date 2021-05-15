a7b2630190cd49809d2f02252f329c85c235a738
Eduardo de Souza Santos Thais <dudu2541999@gmail.com>
2021-05-15 10:22:14 -0300

    Ajustando o repositorio GIt

---
+++ Back/cepapp/src/main/java/com/cepapp/cepapp/CepappApplication.java
@@ -0,0 +1,13 @@
+package com.cepapp.cepapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CepappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CepappApplication.class, args);
+	}
+
+}
---
+++ Back/cepapp/src/test/java/com/cepapp/cepapp/CepappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cepapp.cepapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CepappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
